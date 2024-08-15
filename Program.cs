0efd565bbb5c539b7e842714ec8b196e8c9fad2c
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-08-15 22:07:44 +0900

    a

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NumFlat;
+using OpenBlasSharp;
+
+static class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        Mat<double> a =
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        var s = new Vec<double>(Math.Min(a.RowCount, a.ColCount));
+        var u = new Mat<double>(a.RowCount, a.RowCount);
+        var vt = new Mat<double>(a.ColCount, a.ColCount);
+        var work = new Vec<double>(s.Count - 1);
+
+        fixed (double* pa = a.Memory.Span)
+        fixed (double* ps = s.Memory.Span)
+        fixed (double* pu = u.Memory.Span)
+        fixed (double* pvt = vt.Memory.Span)
+        fixed (double* pwork = work.Memory.Span)
+        {
+            Lapack.Dgesvd(
+                MatrixLayout.ColMajor,
+                'A', 'A',
+                a.RowCount, a.ColCount,
+                pa, a.Stride, // The content of 'a' will be destroyed.
+                ps,
+                pu, u.Stride,
+                pvt, vt.Stride,
+                pwork);
+        }
+
+        Console.WriteLine(u * s.ToDiagonalMatrix() * vt);
+    }
+}
